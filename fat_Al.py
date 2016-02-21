afa11ef7d926c7dc34539a3897d92fc743e8e593
Dennis Sun <Dennis@172-16-8-77.DYNAPOOL.NYU.EDU>
2016-02-21 06:04:21 -0500

    "Final" changes

--- fat_Al.py
+++ fat_Al.py
@@ -1,6 +1,7 @@
 from lxml import etree
 from io import StringIO, BytesIO
 from sched_Class import Schedule
+from random import randint
 
 fileNames = ["Advanced College Essay", "Calculus 2", "Objected Oriented Programming"]
 
@@ -8,16 +9,38 @@ weekdays = {"MON": 0, "TUE":1, "WED":2, "THU":3, "FRI":4, "SAT":5, "SUN":6}
 
 courses = {}
 
+def permute(perms):
+    """
+    Returns list permutations given a list where each element is a list of
+    possible values in that position.
 
-def isTimeCompatible( A, B):
+    perms: list of lists, where each elemental list contains possible values
+        for that position (list<list>)
+    returns: list of permutations, where each permutation is a list
+        (list<list>)
+    """
+    if len(perms) == 1: return [[classNum] for classNum in perms[0]]
+    subperms = permute(perms[1:])
+    newPerms = []
+    for classNum in perms[0]:
+        for subperm in subperms: newPerms.append([classNum] + subperm)
+    return newPerms
+
+def isTimeCompatible(A, B):
     return (A.endTime < B.startTime or B.endTime < A.startTime)
 
-def isDateCompatible(A,B):
+def isDateCompatible(A, B):
     for x in range(len(A.weekdayList)):
-        if(A.weekdayList[x] in B.weekdayList):
-           return False
+        if(A.weekdayList[x] in B.weekdayList): return False
     return True
-    
+
+def isValidSchedule(sched):
+    for i in xrange(1, len(sched)):
+        for j in xrange(i):
+            if not isDateCompatible(sched[i], sched[j]) and \
+            not isTimeCompatible(sched[i], sched[j]): return False
+    return True
+
 for classNameIndex in range(len(fileNames)):
     listOfSchedule = []
     
@@ -56,14 +79,27 @@ for classNameIndex in range(len(fileNames)):
 
     courses[fileNames[classNameIndex]] = listOfSchedule
 
-
-A=courses[fileNames[0]][0]
-B=courses[fileNames[0]][10]
-
-A.printOut()
-B.printOut()
-
-C = isTimeCompatible(A,B)
-D = isDateCompatible(A,B)
-print C
-print D
+if __name__ == "__main__":
+    schedules = permute([courses[courseName] for courseName in fileNames])
+    validSchedules = []
+    for sched in schedules:
+        if isValidSchedule(sched): validSchedules.append(sched)
+    '''
+    for vSched in validSchedules:
+        print("=" * 25 + "\n")
+        for classSched in vSched: classSched.printOut()
+        print("\n" * 2 + "=" * 25)
+    '''
+    print("=" * 25 + "\n")
+    for classSched in validSchedules[randint(0, len(validSchedules))]: classSched.printOut()
+    resp = raw_input("Is this schedule okay? (y/n): ").lower()
+    valid = resp[0] == "y" or resp[0] == "n"
+    while not valid or resp[0] == "n":
+        if not valid:
+            print("Invalid input!")
+        else:
+            print("\n" * 2 + "=" * 25)
+            print("=" * 25 + "\n")
+            for classSched in validSchedules[randint(0, len(validSchedules))]: classSched.printOut()
+        resp = raw_input("Is this schedule okay? (y/n): ").lower()
+        valid = resp[0] == "y" or resp[0] == "n"
