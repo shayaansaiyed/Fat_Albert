8524ac0e87ce01eb687f15d0ec47dd2ea3c3535d
kimmykong <kimmykong@yahoo.com>
2016-02-20 22:16:31 -0500

    working weekdays, int times

--- fat_Al.py
+++ fat_Al.py
@@ -2,19 +2,49 @@ from lxml import etree
 from io import StringIO, BytesIO
 from sched_Class import Schedule
 
+MON = 0
+TUE = 1
+WED = 2
+THU = 3
+FRI = 4
+SAT = 5
+SUN = 6
+
+weekdays = {"MON": 0, "TUE":1, "WED":2, "THU":3, "FRI":4, "SAT":5, "SUN":6}
+
+
 listOfSchedule = []
 
 tree = etree.parse("thing.xml")
 xml_list = tree.findall("class")
 
 
+
+
 for x in range(len(xml_list)):
     start = xml_list[x].find("startTime").text
     end = xml_list[x].find("endtTime").text
-    prof = xml_list[x].find("professor").text
+
+    startTime = 60*int(start[:2]) + int(start[3:5])
+    endTime = 60*int(end[:2]) + int(end[3:5])
+
     wkDays = xml_list[x].find("weekDays").text
+
+    firstWeekDay = wkDays[:3].upper()
+    weekdayList=[]
+    
+    weekdayList.append(weekdays[firstWeekDay])
+
+
+    if len(wkDays) != 3:
+        secondWeekDay = wkDays[4:].upper()
+
+    weekdayList.append(weekdays[secondWeekDay])   
+        
     classNum = xml_list[x].find("classNum").text
-    schedule = Schedule(start, end, prof, wkDays, classNum)
+    prof = xml_list[x].find("professor").text
+    
+    schedule = Schedule(startTime, endTime, prof, classNum,weekdayList)
     listOfSchedule.append(schedule)
 
 for x in range(len(listOfSchedule)):
--- sched_Class.py
+++ sched_Class.py
@@ -1,14 +1,18 @@
 
 
 class Schedule(object):
-    def __init__(self, start, end, prof, dayOfWeek, classNumber):
+    def __init__(self, start, end, prof, dayOfWeek, classNumber,weekdayList):
         self.startTime = start
         self.endTime = end
         self.professor = prof
         self.daysOfWeek = dayOfWeek
         self.classNumber = classNumber
+        self.weekdayList = weekdayList
     def printOut(self):
         print self.classNumber
-        print self.startTime + " - " + self.endTime
+        print self.startTime
+        print self.endTime
         print self.daysOfWeek
         print self.professor
+        print self.weekdayList
+        print "\n"
