9b3e205f0d4990b5312f1372cab447e944aa0e36
kimmykong <kimmykong@yahoo.com>
2016-02-20 22:18:32 -0500

    remove param

--- sched_Class.py
+++ sched_Class.py
@@ -1,18 +1,16 @@
 
 
 class Schedule(object):
-    def __init__(self, start, end, prof, dayOfWeek, classNumber,weekdayList):
+    def __init__(self, start, end, prof, classNumber,weekdayList):
         self.startTime = start
         self.endTime = end
         self.professor = prof
-        self.daysOfWeek = dayOfWeek
         self.classNumber = classNumber
         self.weekdayList = weekdayList
     def printOut(self):
         print self.classNumber
         print self.startTime
         print self.endTime
-        print self.daysOfWeek
         print self.professor
         print self.weekdayList
         print "\n"
